1e6c623c682db6a4d2c82f89fe21b2439c241df0
daniel <d.danielprince2003@gmail.com>
2024-01-23 18:06:38 +0530

    CollegeWork

---
+++ 23-01-2024/src/App.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+/*
+public class App {
+    public static void main(String[] args) throws Exception {
+        double principle,intrest,rate,si;
+        Scanner s = new Scanner(System.in);
+        principle = s.nextDouble();
+        intrest = s.nextDouble();
+        rate = s.nextDouble();
+        si = (principle * intrest * rate )*0.01;
+        System.out.println(si);
+    }
+}
+
+
+
+public class App{
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        double len,bre;
+        len = s.nextDouble();
+        bre = s.nextDouble();
+        double ex_len = 2 * (len + bre);
+        double carper = len * bre;
+        System.out.println((ex_len  + " "  + carper+"sqm"));
+    }
+}
+
+
+
+public class App{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double x1,x2,y1,y2;
+        x1 = s.nextDouble();
+        y1 = s.nextDouble();
+        x2 = s.nextDouble();
+        y2 = s.nextDouble();
+
+        double X = (x1+x2)/2;
+        double Y= (y1+y2)/2;
+        System.out.println(X + " "+ Y);
+    }
+
+}
+
+
+
+
+public class App{
+    public static void main(String args[])
+    {
+        int copy, a,b;
+        Scanner s = new Scanner (System.in);
+        copy  = s.nextInt();
+        a = s.nextInt();
+        b = s.nextInt();
+        double amt = ((copy*a) - (copy * b) - 100);
+        System.out.println(amt);
+    }
+}
+
+
+public class App{
+    public static void main(String args[])
+    {
+        int a,rem,ans=0;
+        Scanner s = new Scanner (System.in);
+        a = s.nextInt();
+        while(a>0)
+        {
+            rem = a%10;
+            ans = ans + rem;
+            a = a / 10;
+        }
+        System.out.println(ans);
+    }
+}
+
+*/
