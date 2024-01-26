683c617534f0ba16fd57c0f786b7c1f06dceaf4b
daniel <d.danielprince2003@gmail.com>
2024-01-26 09:43:57 +0530

    feat:Problem on Looping Series 4 is  Completed

--- Loop-4.java
+++ Loop-4.java
@@ -3,20 +3,20 @@ public class Main
 {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
-    int num,i=7,j=5;
+    int num,i=7,j=5,start=1;
     num = s.nextInt();
-    while(num > 0){
-      if(num%2!=0)
+    while(start <= num ){
+      if(start%2!=0)
       {
         System.out.print(i+" ");
         i++;
       }
-      if(num%2==0)
+      if(start%2==0)
       {
         System.out.print(j+ " ");
         j++;
       }
-      num--;
+      start++;
     }
   }
   
