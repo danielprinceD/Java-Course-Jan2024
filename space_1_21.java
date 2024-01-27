b58eebf74b366e0dd7decbef520674fe48a1e4cc
unknown <admin@RCSE-169.psrengg.ac.in>
2024-01-27 21:47:50 +0530

    feat:patterns with space completed

---
+++ space_1_21.java
@@ -0,0 +1,15 @@
+public class space_1_21 {
+    public static void main(String[] args) throws Exception {
+        for(int i=1;i<=5;i++)
+        {
+            for(int k=5;k>i;k--)
+                System.err.print(" ");
+            
+            for(int j=1;j<=i;j++)
+                System.err.print(j);
+        
+        System.out.println();
+        
+    }
+}
+}
