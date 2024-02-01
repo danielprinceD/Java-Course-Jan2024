be90abe70091c75e59f05700a83c56f7acbbdaf4
unknown <admin@RCSE-169.psrengg.ac.in>
2024-02-01 12:18:38 +0530

    feat:sum of array using for each

--- First_UI/UI/src/App.java
+++ First_UI/UI/src/App.java
@@ -4,14 +4,13 @@ public class App {
     App()
     {
         Frame f = new Frame("Java Desktop GUI");
-        Label l = new Label("Hello World                                   ");
-        f.setTitle("UI Application");
+        Label l = new Label("Hello World");
+
+        l.setAlignment(Label.CENTER);
         f.add(l);
         f.setSize(200,200);
         f.setVisible(true);
-        TextField tx = new TextField("Have a meal ? : ");
-        tx.setVisible(true);
-        f.add(tx);
+
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
         if(num > -1)
