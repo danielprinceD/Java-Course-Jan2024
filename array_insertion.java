ff3937fc3f7a011e5bcfe211e65dfe323fa27e7a
daniel <d.danielprince2003@gmail.com>
2024-02-01 19:19:40 +0530

    Distinct Array completed

--- array_insertion.java
+++ array_insertion.java
@@ -12,7 +12,7 @@ public class array_insertion {
         int ele = s.nextInt();
         for(int i=0;i<size;i++)
          {
-             if(pos-1==i)
+             if(pos-1==i){
                 System.out.print(ele+" ");
                 
             System.out.print(a[i]+" ");
