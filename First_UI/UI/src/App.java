8fd7d6929fff56def16ece0be9698574fff1ce2e
unknown <admin@RCSE-169.psrengg.ac.in>
2024-02-01 11:08:14 +0530

    feat:Created Java GUI

--- First_UI/UI/src/App.java
+++ First_UI/UI/src/App.java
@@ -1,18 +1,21 @@
 import java.awt.*;
+import java.util.Scanner;
 public class App {
     App()
     {
-        Frame f = new Frame();
+        Frame f = new Frame("Java Desktop GUI");
         Label l = new Label("Hello World                                   ");
         f.setTitle("UI Application");
         f.add(l);
-        Button b = new Button("Hello");
-        b.setBounds(20,40,20,40);
-        b.setSize(200,200);
-        b.setVisible(true);
-        f.add(b);
         f.setSize(200,200);
         f.setVisible(true);
+        TextField tx = new TextField("Have a meal ? : ");
+        tx.setVisible(true);
+        f.add(tx);
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        if(num > -1)
+            f.dispose();
 
     }
     public static void main(String[] args) throws Exception {
