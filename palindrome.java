02c7d22fb6c0994312a1b4066f7cf7c78217c4e6
unknown <admin@RCSE-169.psrengg.ac.in>
2024-02-07 18:43:58 +0530

    feat:Palindrome Problem updated

--- palindrome.java
+++ palindrome.java
@@ -3,22 +3,14 @@ import java.util.Scanner;
 public class palindrome {
 
     static int ans= 0;
-    static int reverse(int num)
+    static int reverse(int num,int rev)
     {
-        if(num == 0)
-            return ans;
-        if(num > 0)
-        {
-            
-            ans = ans *10 + (num %10);
-            reverse(num/10);
-        }
-        return ans;
+        return (num == 0)? rev:reverse(num/10, rev*10 + num%10);
     }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
-        int rev = reverse(num);
+        int rev = reverse(num,0);
         System.out.println(rev);
     }
 }
