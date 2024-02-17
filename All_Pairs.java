858fa7933d920fec001c646fd6ff6b8cd8601d18
danielprinceD <d.danielprince2003@gmail.com>
2024-02-17 14:08:18 +0530

    feat:All Pairs in Portal Problem Completed

--- All_Pairs.java
+++ All_Pairs.java
@@ -1,36 +1,25 @@
 import java.util.Scanner;class Pairs{ 
     int num,pair_num; 
     int arr[];
-    Pairs(int num) 
+    Pairs(int num)
     {    
       this.num = num;
       this.arr = new int[num];
      }  
-    void pairs()   
-    {    
-      Scanner s = new Scanner(System.in); 
-         
-      for(int i=0;i<this.num;i++)          
-        this.arr[i] = s.nextInt();    
-      this.pair_num = s.nextInt();    
-     
-    }    
-    void print() 
-    {   
-      for(int i=0;i<this.num;i++)        {      
-        {      
-          for(int j=0;j<this.num;j++)
-           if(this.arr[i] + this.arr[j] == this.pair_num)       
-            System.out.println(this.arr[i] + " " +this.arr[j]);       
-        }    
-      }  
-    }
   }
   public class All_Pairs {  
     public static void main(String[] args) {   
       Scanner s = new Scanner(System.in);   
       int num = s.nextInt();  
       Pairs p = new Pairs(num);
-      p.pairs();
-      p.print();
+      for(int i=0;i<p.num;i++)          
+        p.arr[i] = s.nextInt();    
+      p.pair_num = s.nextInt();
+      for(int i=0;i<p.num;i++)        {      
+        {      
+          for(int j=0;j<p.num;j++)
+           if(p.arr[i] + p.arr[j] == p.pair_num)       
+            System.out.println(Integer.toString(p.arr[i])  + " " +Integer.toString(p.arr[j]));       
+        }    
+      } 
     }}
