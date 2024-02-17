424667cf23e9ff0483347874829fd228fd702649
danielprinceD <d.danielprince2003@gmail.com>
2024-02-17 10:37:36 +0530

    feat:Creating Multi-Threading Process

--- All_Pairs.java
+++ All_Pairs.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 class Pairs
 {
-    
+    int num,pair_num;
+    void pairs()
+    {
+        Scanner s = new Scanner(System.in);
+        int arr[] = new int[num];
+        for(int i=0;i<num;i++)
+            arr[i] = s.nextInt();
+        pair_num = s.nextInt();
+        print(arr , pair_num);
+    }
+    void print(int arr[], int p)
+    {
+        for(int i=0;i<num;i++)
+        {
+            for(int j=i;j<num;j++)
+            {
+                if(arr[i] + arr[j] == p)
+                    System.out.println(arr[i] + " " +arr[j]);
+            }
+        }
+    }
 }
 public class All_Pairs {
     public static void main(String[] args) {
-        
+        Pairs p = new Pairs();
+        Scanner s = new Scanner(System.in);
+        p.num = s.nextInt();
+        p.pairs();
     }
 }
