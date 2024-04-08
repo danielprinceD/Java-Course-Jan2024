a0f4665c92622f596af1f5074dd8234463ae02af
daniel <d.danielprince2003@gmail.com>
2024-04-08 17:15:49 +0530

    completed:Problem 1

---
+++ Lavendel/8-4-2024/1.Add Remove List.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    List<Integer> ll = new LinkedList<>();
+	    ll.add(10);
+	    System.out.println(ll);
+	    ll.remove(0);
+	    System.out.print(ll);
+	}
+}
