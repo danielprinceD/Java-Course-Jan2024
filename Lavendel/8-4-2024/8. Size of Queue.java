0b4c45e464f1dcd9f97e7a00dddcc96ed575fdad
daniel <d.danielprince2003@gmail.com>
2024-04-08 17:43:56 +0530

    completed:Problem 8

---
+++ Lavendel/8-4-2024/8. Size of Queue.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    Queue<Integer> q = new LinkedList<>();
+	   
+	    q.add(10);
+	    q.add(20);
+	    q.add(30);
+	    q.add(40);
+	    
+	    System.out.print(q.size());
+	   
+	}
+}
+
