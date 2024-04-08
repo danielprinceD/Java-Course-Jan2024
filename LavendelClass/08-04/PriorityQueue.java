2af0417091182dc1eac4117c47fa4d7e01c067de
danielprinceD <d.danielprince2003@gmail.com>
2024-04-08 09:50:09 +0530

    PriorityQueue Collection

---
+++ LavendelClass/08-04/PriorityQueue.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Queue<Integer> pq = new PriorityQueue<>();
+	    pq.add(40);
+	    pq.add(30);
+	    pq.add(20);
+	    pq.add(10);
+	    System.out.print(pq);
+	}
+}
