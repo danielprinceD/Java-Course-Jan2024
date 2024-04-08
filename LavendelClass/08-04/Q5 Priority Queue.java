6496062a35c4e1b85ffba661afa799d4e4244c70
danielprinceD <d.danielprince2003@gmail.com>
2024-04-08 11:43:14 +0530

    Problem 5 Completed

--- LavendelClass/08-04/Q5 Priority Queue.java
+++ LavendelClass/08-04/Q5 Priority Queue.java
@@ -5,9 +5,10 @@ public class Main
 	public static void main(String[] args) {
 	   Scanner sc = new Scanner(System.in);
 	   Queue<Integer> q = new PriorityQueue<>();
-	   q.add(30);
-	   q.add(20);
-	   q.add(10);
+	   int size = sc.nextInt();
+	   for(int i=0;i<size;i++)
+	    q.add(sc.nextInt());
+	    
 	   while(q.peek()!=null)
 	       System.out.print(q.poll() + " ");
 	}
