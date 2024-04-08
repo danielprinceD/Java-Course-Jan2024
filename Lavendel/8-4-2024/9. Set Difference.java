614b2ad255f75cdc179bfe74a31c15ac76e3567c
daniel <d.danielprince2003@gmail.com>
2024-04-08 17:47:21 +0530

    completed:Problem 9

---
+++ Lavendel/8-4-2024/9. Set Difference.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    Set<Integer> s1 = new HashSet<>();
+	    Set<Integer> s2 = new HashSet<>();
+	   
+	    int n = sc.nextInt();
+	   for(int i=0;i<n;i++)s1.add(sc.nextInt());
+	   
+	    n = sc.nextInt();
+	   for(int i=0;i<n;i++)s2.add(sc.nextInt());
+	   
+	   s1.removeAll(s2);
+	   System.out.print(s1);
+	}
+}
